0ad08977b06eb22eaeb033a7ef3107fcffcd7ff1
Julien Phalip <jphalip@gmail.com>
2024-06-08 15:47:07 -0700

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.16.0"
-    id("com.diffplug.spotless") version "6.15.0"
+    kotlin("jvm") version "1.9.23"
+    id("org.jetbrains.intellij") version "1.17.3"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 repositories {
@@ -9,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("com.google.guava:guava:31.1-jre")
+    testImplementation("com.google.guava:guava:33.2.1-jre")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
@@ -17,20 +18,23 @@ dependencies {
 spotless {
     java {
         removeUnusedImports()
-        googleJavaFormat("1.8").aosp().reflowLongStrings().groupArtifact("com.google.googlejavaformat:google-java-format")
+        googleJavaFormat("1.22.0")
+            .aosp()
+            .reflowLongStrings()
+            .groupArtifact("com.google.googlejavaformat:google-java-format")
     }
 }
 
 intellij {
-    version.set("2022.1.4")
+    version.set("2024.1.2")
     type.set("IC")
     plugins.set(listOf("java"))
 }
 
 tasks {
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
 
     signPlugin {
